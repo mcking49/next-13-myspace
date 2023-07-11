55e50217c47ab61ca1c91742798f939da8d50d2c
Miten Chauhan <miten@chauhan.nz>
2023-07-11 22:10:32 +1000

    Auth data store

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
-import { NextAuthOptions } from 'next-auth'
+import { prisma } from '@/lib/prisma'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import type { NextAuthOptions } from 'next-auth'
 import NextAuth from 'next-auth/next'
 import GithubProvider from 'next-auth/providers/github'
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { getServerSession } from 'next-auth'
 import styles from './page.module.css'
 import { redirect } from 'next/navigation'
+import { authOptions } from './api/auth/[...nextauth]/route'
 
 export default async function Home() {
-  const session = await getServerSession()
+  const session = await getServerSession(authOptions)
 
   if (!session) {
     redirect('/api/auth/signin')
