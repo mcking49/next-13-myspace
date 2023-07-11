7ac97d24a42e94c0a6ef0a6c1265cbf2780ebe98
Miten Chauhan <miten@chauhan.nz>
2023-07-11 13:57:14 +1000

    SSR

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,28 @@
+interface Post {
+  title: string
+  content: string
+  slug: string
+}
+
+interface Props {
+  params: { slug: string }
+}
+
+export default async function BlogPostPage({ params }: Props) {
+  const posts: Post[] = await fetch('http://localhost:3000/api/content').then(
+    (res) => res.json(),
+  )
+
+  const post = posts.find((post) => post.slug === params.slug)
+
+  if (!post) {
+    throw new Error('Post not found')
+  }
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  )
+}
