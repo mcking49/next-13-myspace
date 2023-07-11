efc7234a7de4346f930ee7fe863c4d2a6e09dcf7
Miten Chauhan <miten@chauhan.nz>
2023-07-11 14:35:02 +1000

    Protect routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import { getServerSession } from 'next-auth'
 import styles from './page.module.css'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession()
+
+  if (!session) {
+    redirect('/api/auth/signin')
+  }
+
   return <main className={styles.main}></main>
 }
