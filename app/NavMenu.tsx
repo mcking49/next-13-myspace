5d3c7fcfae58e158e5bed57f03274165c44b0189
Miten Chauhan <miten@chauhan.nz>
2023-07-11 14:32:10 +1000

    Sign in buttons

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import styles from './NavMenu.module.css'
 import Link from 'next/link'
+import { SignInButton, SignOutButton } from '@/components/buttons'
+import AuthCheck from '@/components/AuthCheck'
 
 export default function NavMenu() {
   return (
@@ -13,12 +15,24 @@ export default function NavMenu() {
         <li>
           <Link href="/about">About</Link>
         </li>
+
         <li>
           <Link href="/blog">Blog</Link>
         </li>
+
         <li>
           <Link href="/users">Users</Link>
         </li>
+
+        <li>
+          <SignInButton />
+        </li>
+
+        <AuthCheck>
+          <li>
+            <SignOutButton />
+          </li>
+        </AuthCheck>
       </ul>
     </nav>
   )
