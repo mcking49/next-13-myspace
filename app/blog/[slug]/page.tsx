1fd02b72511b748e2970cc295e939f5fc8c8d570
Miten Chauhan <miten@chauhan.nz>
2023-07-11 14:00:19 +1000

    Static generation

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 420
+
 interface Post {
   title: string
   content: string
@@ -6,6 +8,16 @@ interface Post {
 
 interface Props {
   params: { slug: string }
+}
+
+export async function generateStaticParams() {
+  const posts: Post[] = await fetch('http://localhost:3000/api/content').then(
+    (res) => res.json(),
+  )
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }))
 }
 
 export default async function BlogPostPage({ params }: Props) {
